101f5497a202234ad3b20dc5c529ca8cf3b365eb
JosephAdeoye <Joseph_adeoye@yahoo.com>
2024-02-29 16:34:36 +0100

    Went through the content of the entire day on Angela Yu's course

--- Day 1.py
+++ Day 1.py
@@ -1,1 +1,22 @@
-print(5 + 7)
+print("Hello!\nI am Adeoye Joseph")
+
+# The \n above pushes the string that comes after to the next line.
+
+
+print("Hello" + " " + "Adeoye")
+
+# The above is an example of string concatenation
+
+input('What is your name?')
+
+# The input function allows us to take in information
+
+len(input("Enter your name "))
+
+# The len function allows to get the number of text in a string.
+
+name = input('What is your name?')
+print('His name is' + ' ' + name)
+# in line 18, we stored the input of the function in the name variable
+# in line 19, we concatenated the string "His name is" with the value of the name variable.
+
