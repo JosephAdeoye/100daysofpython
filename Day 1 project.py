b671efc94a01500452f026f2efc3b2c838ca8e47
JosephAdeoye <Joseph_adeoye@yahoo.com>
2024-02-29 16:54:46 +0100

    Completed day 1 project

---
+++ Day 1 project.py
@@ -0,0 +1,20 @@
+# 1. Create a greeting for your program
+
+# 2. Ask the user for the city that they grew up in.
+
+# 3. Ask the user for the name of a pet
+
+# 4. Combine the name of their city and pet and show them their band name.
+
+# 5. Make sure the input cursor shows on a new line.
+
+print('Hello, welcome to my band name generator!')
+# the line of code above satisfies the first rule
+
+city = input('What city did you grow up in?\n')
+# the \n above ensures that the input is typed on a new line. This satisfies the fifth rule
+
+pet = input('What is the name of your pet?\n')
+
+print('Your Band Name is ' + city + ' ' + pet)
+
