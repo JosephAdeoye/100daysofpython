add0ac9423ec4005c11422ac99702f3d2c736761
JosephAdeoye <Joseph_adeoye@yahoo.com>
2024-03-04 23:21:58 +0100

    Started Day 3 and started learning about conditional statements

--- Day 3.py
+++ Day 3.py
@@ -13,4 +13,30 @@ else:
 # >= greater than or equals to
 # <= less than or equals to
 # == equal to
-# != not equal to 
+# != not equal to
+# % gives the remainder after a division
+
+if height >= 120:
+    print('You are allowed to ride the rollercoaster')
+    age = int(input('How old are you? '))
+    if age <= 18:
+        print('Please pay $7.')
+    else:
+        print('Please pay $12.')
+else:
+    print('Sorry, you need to grow taller to ride the rollercoaster')
+
+# # the above is an example of nesting one if else conditional in another
+
+if height >= 120:
+    print('You are allowed to ride the rollercoaster')
+    age = int(input('How old are you? '))
+    if age < 12:
+        print('Please pay $5.')
+    elif age <= 18:
+        print('Please pay $7.')
+    else:
+        print('Please pay $12.')
+else:
+    print('Sorry, you need to grow taller to ride the rollercoaster')
+# in the above we introduced the elif which is the else-if conditional. We can have as much elif between if-else.
