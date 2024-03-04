fd6787fe32c3351d24a47783603cb4d8a01f7613
JosephAdeoye <Joseph_adeoye@yahoo.com>
2024-03-04 22:15:52 +0100

    Started Day 3 and started learning about conditional statements

---
+++ Day 3 project.py
@@ -0,0 +1,8 @@
+number = int(input('What number do you want to check? '))
+
+if number % 2 == 0:
+    print('This is an even number.')
+else:
+    print('This is an odd number.')
+
+# the % modulo operator here gives us the remainder after a division.
