6740b86c0e357e0d0e7abc41c80b51065cc1595f
JosephAdeoye <Joseph_adeoye@yahoo.com>
2024-03-04 21:48:31 +0100

    Started Day 3 and started learning about conditional statements

---
+++ Day 3.py
@@ -0,0 +1,16 @@
+# control flow with if_else and conditional operators
+print('Welcome to the Rollercoaster!')
+height = int(input('What is your height in centimetres? '))
+
+if height >= 120:
+    print('You are allowed to ride the rollercoaster')
+else:
+    print('Sorry, you need to grow taller to ride the rollercoaster')
+# the above shows conditional statements at it's basic form
+
+# > greater than
+# < less than
+# >= greater than or equals to
+# <= less than or equals to
+# == equal to
+# != not equal to 
