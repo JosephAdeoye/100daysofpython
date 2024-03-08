fd0362b9707ed2b1957caab86a1f86b4b2e104a9
JosephAdeoye <Joseph_adeoye@yahoo.com>
2024-03-08 15:41:13 +0100

    Started Love calculator project and renamed pizza order project

---
+++ Day 3 - Love calculator project.py
@@ -0,0 +1,8 @@
+print('Welcome to the Love Calculator!')
+name1 = input('What is your name? \n')
+name2 = input('What is their name? \n')
+
+lower_case_name1 = name1.lower()
+lower_case_name2 = name2.lower()
+
+print(lower_case_name1)
