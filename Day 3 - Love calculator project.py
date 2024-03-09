550c247a4c4f37fdb0291622825d1f2a5c0ddf55
JosephAdeoye <Joseph_adeoye@yahoo.com>
2024-03-09 23:58:13 +0100

    continued the Love calculator project

--- Day 3 - Love calculator project.py
+++ Day 3 - Love calculator project.py
@@ -5,4 +5,19 @@ name2 = input('What is their name? \n')
 lower_case_name1 = name1.lower()
 lower_case_name2 = name2.lower()
 
-print(lower_case_name1)
+# Count the total occurrences of 'true love' characters in the name
+total_occurrences_name1 = lower_case_name1.count('t') + lower_case_name1.count('r') + lower_case_name1.count('u') + \
+                            lower_case_name1.count('e') + lower_case_name1.count('l') + lower_case_name1.count('o') + \
+                            lower_case_name1.count('v') + lower_case_name1.count('e')
+
+total_occurrences_name2 = lower_case_name2.count('t') + lower_case_name2.count('r') + lower_case_name2.count('u') + \
+                            lower_case_name2.count('e') + lower_case_name2.count('l') + lower_case_name2.count('o') + \
+                            lower_case_name2.count('v') + lower_case_name2.count('e')
+
+print(total_occurrences_name1,  total_occurrences_name2)
+
+
+
+
+
+
