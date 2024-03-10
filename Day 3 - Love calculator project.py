c0d7a567c467f4f6fee4f05fff1e8cb35ed2cad8
JosephAdeoye <Joseph_adeoye@yahoo.com>
2024-03-10 21:43:54 +0100

    continued the Love calculator project

--- Day 3 - Love calculator project.py
+++ Day 3 - Love calculator project.py
@@ -14,7 +14,19 @@ total_occurrences_name2 = lower_case_name2.count('t') + lower_case_name2.count('
                             lower_case_name2.count('e') + lower_case_name2.count('l') + lower_case_name2.count('o') + \
                             lower_case_name2.count('v') + lower_case_name2.count('e')
 
-print(total_occurrences_name1,  total_occurrences_name2)
+final_occurrence_name1 = str(total_occurrences_name1)
+final_occurrence_name2 = str(total_occurrences_name2)
+
+love_level = int(final_occurrence_name1) + int(final_occurrence_name2)
+print(love_level)
+
+# if love_level >= 40:
+#     print('lol')
+# else:
+#
+
+
+
 
 
 
