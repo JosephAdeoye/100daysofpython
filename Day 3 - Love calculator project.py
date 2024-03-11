bae77bbe2fe30247e679ca2f4c2ac18b70fe6b48
JosephAdeoye <Joseph_adeoye@yahoo.com>
2024-03-11 23:49:16 +0100

    finished the Love calculator project

--- Day 3 - Love calculator project.py
+++ Day 3 - Love calculator project.py
@@ -13,15 +13,21 @@ e = lower_string_name.count('e')
 
 true = t + r + u + e
 
-l = lower_string_name.count('l')
+L = lower_string_name.count('l')
 o = lower_string_name.count('o')
 v = lower_string_name.count('v')
 e = lower_string_name.count('e')
 
-love = l + o + v + e
+love = L + o + v + e
 
-love_score = str(true) + str(love)
+love_score = int(str(true) + str(love))
 
+if love_score < 10 or love_score > 90:
+    print(f'Your love score is {love_score}, you go together like coke and mentos.')
+elif love_score >= 40 and love_score <= 50:
+    print(f'Your love score is {love_score}, you are alright together.')
+else:
+    print(f'Your score is {love_score}')
 
 
 
