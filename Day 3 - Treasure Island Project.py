c6b58fa4ea2b0c6891c3ed0a138dce30c47ddc95
JosephAdeoye <Joseph_adeoye@yahoo.com>
2024-03-12 02:29:24 +0100

    written the most part of the logic but there are still some things to fix

--- Day 3 - Treasure Island Project.py
+++ Day 3 - Treasure Island Project.py
@@ -22,4 +22,28 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print('Welcome to Treasure Island!')
 print('Your objective is to find the Treasure.')
-direction = input('You are at a cross road. Where do you want to go? Type "left" or "right"')
+direction = input('You are at a cross road. Where do you want to go? Type "left" or "right" \n')
+what_direction = direction.lower()
+
+if what_direction != 'left':
+    print('You fell into a manhole, Game Over!')
+# else:
+
+swim_wait = input('You have come to a slow flowing river, What do you want to do? Type "swim" or "wait" \n')
+swim_or_wait = swim_wait.lower()
+
+if swim_or_wait != 'wait':
+    print('You were attacked by a crocodile, Game Over')
+
+door = input('You are faced with three doors. Which do you enter? Type "blue", "yellow", "red" \n')
+which_door = door.lower()
+
+if which_door == 'yellow':
+    print('Congratulations, you have located the treasure, You Win!')
+elif which_door == 'blue':
+    print('You were eaten by beasts, Game Over')
+elif which_door == 'red':
+    print('You fell off a cliff, Game Over')
+else:
+    print('Game Over')
+
