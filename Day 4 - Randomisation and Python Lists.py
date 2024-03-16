a4521cef826b9afe0b1707a66f667c0b29e2a2c0
JosephAdeoye <Joseph_adeoye@yahoo.com>
2024-03-16 17:36:04 +0100

    More on Day 4

--- Day 4 - Randomisation and Python Lists.py
+++ Day 4 - Randomisation and Python Lists.py
@@ -7,3 +7,26 @@ print(random_number)
 random_float = random.uniform(0.0, 5.0)
 
 print(random_float)
+
+# Python lists
+# lists can contain any type of data
+# it is also important to note that lists have orders and the other is not lost when the list is stored in a variable
+
+fruits = ['apple', 'orange', 'pawpaw', 'cucumber', 'date', 'coconut']
+
+print(fruits[0])
+
+# you can have positive or negative index
+
+fruits[1] = 'oranjen'
+
+# you can also select an item in the list and change it
+
+fruits.append('cashew')
+
+# using the append function, we can add an item to the end of our list.
+
+# if you have more than one item, you can use the extend function. See below
+
+fruits.extend(['mango', 'pineapple'])
+
