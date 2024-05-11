5953be1c5345c1359a35efcf467885c5f6ac185b
JosephAdeoye <Joseph_adeoye@yahoo.com>
2024-05-12 00:15:45 +0100

    Completed Rock, Paper & Scissors game.

--- Day 4 - Rock, Paper & Scissors Final Project.py
+++ Day 4 - Rock, Paper & Scissors Final Project.py
@@ -24,31 +24,53 @@ scissors = '''
       (____)
 ---.__(___)  
 '''
+
+user_choice = int(input('What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. '))
+
+if user_choice == 0:
+    print(f"You chose: \n {rock}")
+elif user_choice == 1:
+    print(f"You chose: \n {paper}")
+elif user_choice == 2:
+    print(f"You chose: \n {scissors}")
+
+computer_choice = random.randint(0, 2)
+if computer_choice == 0:
+        print(f"Computer chose: \n {rock}")
+elif computer_choice == 1:
+        print(f"Computer chose: \n {paper}")
+elif computer_choice == 2:
+        print(f"Computer chose: \n {scissors}")
+
 #   Rules ---
 #   Rock beats scissors (rock crushes scissors)
 #   Scissors beat paper (scissors cut paper)
 #   Paper beats rock (paper covers rock)
 
-choice = int(input('What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.' ))
+# 0 for Rock, 1 for Paper or 2 for Scissors.
 
-if choice == 0:
-    print(rock)
-elif choice == 1:
-    print(paper)
-elif choice == 2:
-    print(scissors)
+if computer_choice == 0 and user_choice == 2:
+    print('You lost, computer wins')
+elif user_choice == 0 and computer_choice == 2:
+    print('You won, computer lost')
 
-random_choice = random.randint(0, 2)
-if random_choice == 0:
-        print(f"Computer chose: \n {rock}")
-elif random_choice == 1:
-        print(f"Computer chose: \n {paper}")
-elif random_choice == 2:
-        print(f"Computer chose: \n {scissors}")
-    # print(f"Computer chose \n {random_choice}")
+if computer_choice == 2 and user_choice == 1:
+    print('You lost, computer wins')
+elif user_choice == 2 and computer_choice == 1:
+    print('You won, computer lost')
 
-# paper = 1
-# scissors = 2
+if computer_choice == 1 and user_choice == 0:
+    print('You lost, computer wins')
+elif user_choice == 1 and computer_choice == 0:
+    print('You won, computer lost')
+
+else:
+    print('It is a draw. Play Again!')
+
+
+
+
+
 
 
 
