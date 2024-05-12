875b5a1f555ebfba985eb5ece1b4c83d7231afaf
JosephAdeoye <Joseph_adeoye@yahoo.com>
2024-05-12 23:47:44 +0100

    added another way to complete 5.3 mini-project

--- Day 5.3 exercise.py
+++ Day 5.3 exercise.py
@@ -3,5 +3,10 @@ for even in range(0, 101, 2):
     sum_even = sum_even + even
 print(f"The sum of all even numbers from 0 to 100 is {sum_even}")
 
+# another way to implement this is
 
-
+total = 0
+for number in range(0, 101):
+    if number % 2 == 0:
+        total += number
+print(f"The sum of all even numbers from 0 to 100 is {total}")
