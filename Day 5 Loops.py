140e7685a7e0eba1861d0ff9714da13739eb9d46
JosephAdeoye <Joseph_adeoye@yahoo.com>
2024-05-12 23:35:44 +0100

    Completed day 5.3 mini-project

--- Day 5 Loops.py
+++ Day 5 Loops.py
@@ -2,3 +2,8 @@ clubs = ['Chelsea, Arsenal, Bayern,  Dortmund']
 for club in clubs:
     print(club)
     print(club + ' FC')
+
+#For loop with range
+for number in range(1, 11, 2):
+    print(number)
+
