d27075344852f9c1a5fd5c13fb93e63ec8e1b6b9
JosephAdeoye <Joseph_adeoye@yahoo.com>
2024-05-15 15:20:01 +0100

    started the hangman game

--- Day 7.py
+++ Day 7.py
@@ -1,10 +1,24 @@
+import random
+
 word_list = ['aardvark', 'baboon', 'camel']
 
 #  Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 #
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
 #  Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+guess = input('Guess a letter\n').lower()
+
 #
 #  Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-#
-#
+for letter in chosen_word:
+    if letter == guess:
+        print('Yes')
+    else:
+        print('No')
+
+
+
 
