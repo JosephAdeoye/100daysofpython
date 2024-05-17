3ab8547c6396eda842cd221a32c5e54f7258494d
JosephAdeoye <Joseph_adeoye@yahoo.com>
2024-05-17 13:09:23 +0100

    Still working on the Hangman game

--- Day 7.py
+++ Day 7.py
@@ -5,34 +5,31 @@ chosen_word = random.choice(word_list)
 # Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-#  Todo-1: - Create an empty list called display. For each letter in the chosen_word,
-#   add a '_' to 'display'
-#  So if the chosen word was 'apple', display should be ['_', '_', '_', '_', '_'] with 5 '_'
-#  representing each letter to guess.
 
 display = []
-for letter in chosen_word:
+for _ in chosen_word:
     display.append('_')
 print(display)
-guess = input('Guess a letter\n').lower()
 
-#   Todo-2: - Loop through each position in the chosen_word;
-#   If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#   e.g. If the user guessed 'p' and the chosen word was 'apple', then display should be ['_', 'p', 'p', '_', '_',].
+# TODO-1 - Use a while loop to let the user guess again. The loop should only stop once the user
+# has guessed all the letters in the chosen_word and 'display has no more blanks '_'.
+# Then you can tell the user they have won.
 
-for i in range(len(chosen_word)):
-    if chosen_word[i] == guess:
-        display[i] == guess
-#
-# for letter in chosen_word:
-#     if letter == guess:
-#         print('Yes')
-#     else:
-#         print('No')
-#
-# #   Todo-3: - Print 'display' and you should see the guessed letter in the correct position
-# #   and every other letter replace with '_'.
-#
-#
-#
+end_of_game = False
+
+
+while not end_of_game:
+    guess = input('Guess a letter\n').lower()
+
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        print(f'current position: {position} current letter: {letter} guessed letter: {guess}')
+        if letter == guess:
+            display[position] = letter
+    print(display)
+
+    if '_' not in display:
+        end_of_game = True
+        print('You win')
+
 
