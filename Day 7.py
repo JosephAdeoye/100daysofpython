a2d7ad611163a9adc291f21edb208cade0df1c2c
JosephAdeoye <Joseph_adeoye@yahoo.com>
2024-05-17 13:09:49 +0100

    Still working on the Hangman game

--- Day 7.py
+++ Day 7.py
@@ -11,7 +11,7 @@ for _ in chosen_word:
     display.append('_')
 print(display)
 
-# TODO-1 - Use a while loop to let the user guess again. The loop should only stop once the user
+# Use a while loop to let the user guess again. The loop should only stop once the user
 # has guessed all the letters in the chosen_word and 'display has no more blanks '_'.
 # Then you can tell the user they have won.
 
