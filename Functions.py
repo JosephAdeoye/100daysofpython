232969fdb3774fef03c71fbbc5ea9982a7c51f21
JosephAdeoye <Joseph_adeoye@yahoo.com>
2024-06-12 08:54:32 +0100

    Learning more about functions

---
+++ Functions.py
@@ -0,0 +1,12 @@
+#   function = a block of reusable code
+#   place a () after the function name to invoke it
+
+def happy_birthday(name, age):
+    print(f'Happy Birthday to {name}!')
+    print(f'You are {age}!')
+    print('Happy Birthday to you!')
+    print()
+
+happy_birthday('Taiwo', 20)
+happy_birthday('AY', 22)
+happy_birthday('BEN', 31)
