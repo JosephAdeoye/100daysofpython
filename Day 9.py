0b174f906453c6d9c330ffb1a149d6902cb1e241
JosephAdeoye <Joseph_adeoye@yahoo.com>
2025-01-15 02:03:57 +0100

    Started day 9 to learn about dictionaries and nesting

---
+++ Day 9.py
@@ -0,0 +1,28 @@
+# Dictionaries in python work similarly to dictionaires in real life. 
+
+#  {key: value}
+
+programming_dictionary = {
+    "bug": "An error in a program that prevents the program from running as expected.",
+    "function": "A piece of code that you can easily call over and over again.",
+    "loop": "The action of doing something over and over again."
+}
+
+# retrieving an item from a dictionary
+print(programming_dictionary["bug"])
+
+# adding a new item to a dictionary
+
+programming_dictionary["recursion"] = "A function that calls itself."
+
+# creating an empty dictionary
+
+empty_dictionary = {}
+
+# wiping an existing dictionary
+
+programming_dictionary = {}
+
+# editing an item in a dictionary
+
+programming_dictionary["bug"] = "A moth in your computer."
